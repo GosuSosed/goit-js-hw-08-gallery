e4704eff945d266086e3ffad5d5f2623dc3b0063
Serhii Prokopchuk <gosusosed@gmail.com>
2021-04-13 00:03:47 +0300

    делает функцию разметки

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-import refs from './gallery-items.js';
-console.log(refs);
+import images from './gallery-items.js';
+console.log(images);
+
+const listRef = document.querySelector('.gallery');
+const createEl = createImagesMarkup(images);
+console.log(createEl);
+// listRef.insertAdjacentElement('beforeend', createEl);
+
+function createImagesMarkup(array) {
+    return array.map( ({preview, original, description}) => { 
+    `<li class="gallery__item">
+      <a
+      class="gallery__link"
+      href="${original}"
+      >
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+      </a>
+    </li>
+    `}).join('');
+};

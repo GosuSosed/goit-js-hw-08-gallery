992272005e52c3cd3f6ae9d29c2f04b76328b688
Serhii Prokopchuk <gosusosed@gmail.com>
2021-04-15 00:24:02 +0300

    заканчивает галерею

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ import galleryItems from './gallery-items.js';
 
 const refs = {
   galleryContainer: document.querySelector('.js-gallery'),
+  galleryImg: document.querySelector('.gallery__image'),
+  modalWindow: document.querySelector('.js-lightbox'),
+  lightBoxImg: document.querySelector('.lightbox__image'),
+  lightBoxOverlay: document.querySelector('.lightbox__overlay'),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
 }
 
 const createEl = createGalleryMarkup(galleryItems);
@@ -25,8 +30,16 @@ function createGalleryMarkup (images) {
     `
   }).join('');
 }
+
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-refs.galleryContainer.addEventListener('click', onImageClick)
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image
+
+refs.galleryContainer.addEventListener('click', onImageClick);
+
+function onModalOpen () {
+  refs.modalWindow.classList.add('is-open');
+}
 
 function onImageClick (evt) {
   evt.preventDefault();
@@ -34,4 +47,32 @@ function onImageClick (evt) {
     return;
     }
 
+    const originalUrl = evt.target.dataset.source;
+    const imageAlt = evt.target.alt;
+    
+    onModalOpen();
+    refs.lightBoxImg.src = originalUrl;
+    refs.lightBoxImg.alt = imageAlt;
+}
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
+// Закрытие модального окна по клику на div.lightbox__overlay
+
+refs.closeBtn.addEventListener('click', onModalClose);
+refs.lightBoxOverlay.addEventListener('click', onModalClose);
+
+function onModalClose () {
+  refs.modalWindow.classList.remove('is-open');
+  
+  refs.lightBoxImg.src = '';
+  refs.lightBoxImg.alt = '';
+}
+
+// Закрытие модального окна по нажатию клавиши ESC
+document.addEventListener('keydown', closeWithEsc);
+
+function closeWithEsc (e) {
+ if(e.code === "Escape") {
+   onModalClose();
+ };
 }
